7469de4843c7f8a8891f3d146218002b98a833ab
Nathan Schelin <n.schelin@hotmail.com>
2019-06-03 22:57:03 -0700

    updated dinner details

--- client/src/components/DinnerDetails.vue
+++ client/src/components/DinnerDetails.vue
@@ -1,38 +1,62 @@
 <template>
 	<div class="column" v-if="dinner !== null">
 		<form @submit.prevent="saveDinner()">
-			<b-field label="Dinner Name">
-				<b-input v-model="dinner.name" ref="dinnerName"></b-input>
-                <div>{{ total | currency }}</div>
-			</b-field>
-            <div class="column is-clearfix">
-                <b-button type="is-primary is-pulled-right" @click="dinner.products.push({})">Add Product</b-button>
-            </div>
-            
-			<div class="field" v-for="(product, index) in dinner.products" :key="product._id">
-				<label class="label">Product</label>
-				<div class="control">
-					<b-select placeholder="Select Product" v-model="dinner.products[index]">
-                        <option v-for="product in products" :value="product" :key="product._id">
-                            {{ product.name }}
-                        </option>
-					</b-select>
-				</div>
-                <div class="control">
-                   Cost {{ product.price | currency }}
+            <div class="columns">
+                <div class="column">
+                    <b-field label="Dinner Name">
+                        <b-input v-model="dinner.name" ref="dinnerName"></b-input>
+                    </b-field>
                 </div>
-			</div> 
+                <div class="column">
+                    <div class="columns is-mobile">
+                        <div class="column" v-if="total > 0">
+                            <label class="label">Cost</label>
+                            <div>
+                                {{ total | currency }}
+                            </div>
+                        </div>
+                        <div class="column">
+                            <b-button type="is-primary is-pulled-right" @click="dinner.products.push({})">Add Product</b-button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-			<div class="column is-clearfix">
+            <h3 class="subtitle">Products</h3>
+            
+            <div class="box" v-if="dinner.products.length > 0">
+                <div class="columns is-mobile" v-for="(product, index) in dinner.products" :key="index">
+                    <div class="column is-6">
+                        <b-field label="Product">
+                            <b-select placeholder="Select Product" v-model="dinner.products[index]">
+                                <option v-for="product in products" :value="product" :key="product._id">
+                                    {{ product.name }}
+                                </option>
+                            </b-select>
+                        </b-field>
+                    </div>
+                    <div class="column is-4">
+                        <label class="label">Product Cost</label>
+                        <div>
+                            {{ product.price | currency }}
+                        </div>
+                    </div>
+                    <div class="column is-2">
+                        <b-button type="is-danger" outlined @click="dinner.products.splice(index, 1)">Remove</b-button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column is-clearfix">
                 <div class="buttons is-pulled-right">
-                    <b-button type="is-primary" native-type="submit">
+                    <b-button type="is-primary" native-type="submit" :disabled="!dinner.name">
                         Save
                     </b-button>
                     <b-button @click="$parent.close()">
                         Cancel
                     </b-button>
                 </div>
-			</div>
+            </div>
 		</form>
 	</div>
 </template>
@@ -49,7 +73,7 @@
             ...mapState({
                 products: state => state.productsMod.products
             }),
-            total: function() { return this.dinner !== null ? this.dinner.products.reduce((sum, product) => sum += product.price, 0) : 0; }
+            total: function() { return this.dinner !== null ? this.dinner.products.reduce((sum, product) => sum += (product.price ? product.price : 0), 0) : 0; }
         },
 		methods: {
 			saveDinner() {
@@ -75,8 +99,11 @@
 </script>
 
 <style lang="scss" scoped>
-
-	// .buttons {
-	// 	float: right;
-	// }
+    .product-container {
+        max-height: 500px;
+        overflow-y: scroll;
+        border: 1px solid lightgray ;
+        border-radius: 3px;
+        padding: 8px;
+    }
 </style>
