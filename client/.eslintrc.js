e6bdb93e5f0f73db7e3260636f053ef8d7938647
Nate S <n.schelin@hotmail.com>
2019-06-07 09:28:47 -0700

    updated eslint for client

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': ['error', { singleQuote: true }]
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ['plugin:vue/essential', '@vue/prettier'],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'prettier/prettier': ['error', { singleQuote: true }],
+		'prettier/prettier': ['error', { useTabs: true }]
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 };
 
 // // http://eslint.org/docs/user-guide/configuring
