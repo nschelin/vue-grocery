6501f5c3b5471e14e8f74a434540fbff8af5b1c1
nschelin <6097334+nschelin@users.noreply.github.com>
2022-10-12 22:49:05 -0700

    add sass pkg; add router, styles, views

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+import '@/styles/main.scss'
+import * as bootstrap from 'bootstrap';
+import router from '@/router';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
